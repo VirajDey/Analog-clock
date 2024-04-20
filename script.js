4bf7db0e5b59688efd8aa8c53b8b6e056960b15d
Viraj Dey <viraj65.dey@gmail.com>
2024-04-20 17:48:27 +0530

    hogaya

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nh = document.querySelector('.number-hours');
+const bs = document.querySelector('.bar-seconds');
 const ne = [];
+const be = [];
 
 //create hours
 for (let i = 1; i <= 12; i++) {
@@ -10,3 +12,30 @@ for (let i = 1; i <= 12; i++) {
 }
 
 nh.insertAdjacentHTML("afterbegin", ne.join(""));
+
+//create seconds
+for (let i = 1; i <= 60; i++) {
+    be.push(
+        `<span style = "--index:${i};"><p></p></span>`
+    );
+
+}
+bs.insertAdjacentHTML("afterbegin", be.join(""));
+
+const hh = document.querySelector(".hands.hours");
+const hm = document.querySelector(".hands.minutes");
+const hs = document.querySelector(".hands.seconds");
+
+function gettime() {
+    let date = new Date();
+    let currenthours = date.getHours();
+    let currentminutes = date.getMinutes();
+    let currentseconds = date.getSeconds();
+    hs.style.transform = `rotate(${currentseconds * 6}deg)`;
+    hm.style.transform = `rotate(${currentminutes * 6}deg)`;
+    hh.style.transform = `rotate(${currenthours * 30 + currentminutes / 2}deg)`;
+}
+
+gettime();
+
+setInterval(gettime,1000);
